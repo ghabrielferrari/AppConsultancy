6312968df1a2ca06c04e0880d5dd505bd2b61c33
Gabriel Ferrari <ghcferrari75@gmail.com>
2024-06-02 22:12:48 -0300

    Initial Commit

---
+++ app/src/main/java/com/gabrielferrari/appconsultancy/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package com.gabrielferrari.appconsultancy.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
